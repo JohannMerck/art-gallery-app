0bc8498a89afee06762012177ea5122b39fc6555
Daniel <ddorow@gmx.de>
2024-08-14 15:03:21 +0200

    add Favorite Page

--- components/Navigation.js
+++ components/Navigation.js
@@ -10,6 +10,9 @@ export default function Navigation() {
         <li>
           <Link href="/art-pieces">Pieces</Link>
         </li>
+        <li>
+          <Link href="/favorites">Favorites</Link>
+        </li>
       </ul>
     </nav>
   );
